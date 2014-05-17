f3c7a6bcf614210e9642d87cbd7e761cac219c29
Chirag Choudhary <chirag5624@gmail.com>
2014-05-17 19:28:13 +0530

    socket.io example

--- NodeJs/sample.js
+++ NodeJs/sample.js
@@ -1,15 +1,38 @@
+var http=require('http');
 var express=require("express");
+var socket=require('socket.io');
 var app=express();
+var fs=require('fs');
 app.use(express.json());
 app.use(express.urlencoded());
-app.post('/',function(req,res){
-	if(req.method=='POST'){
-		console.log("Your name is "+req.body.usr);
-				}
-	res.send(404,"Hello User");
-	
-	
-	
+var server=http.createServer(app);
+app.get('/',function(req,res){
+
+console.log('URL:'+req.url);
+res.send('Welcome User!!');
 });
+
+app.post('/',function(req,res){
+var data='';
+req.on('data',function(chunk){data+=chunk;});
+req.on('end',function(){
+
+console.log(data);
+res.end('Data Received!!');
+		});
+});
+
+app.get('/socket.html',function(req,res){
+fs.readFile(__dirname+'/socket.html',function(error,data){
+
+res.writeHead(200,{'Content-type':'text/html'});
+res.end(data,'UTF-8');
+	});
+});
+
+	
+	
+
 console.log("Listening on port 8080");
-app.listen(8080,"127.0.0.1");
+server.listen(8080,'127.0.0.1');
+socket.listen(server);
